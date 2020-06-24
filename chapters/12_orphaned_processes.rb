513508bd94785361750688f77e103d353239495e
pettil <lucas.petti@devolute.org>
2020-06-24 19:28:23 +0200

    Include chapter 12

--- chapters/12_orphaned_processes.rb
+++ chapters/12_orphaned_processes.rb
@@ -2,4 +2,10 @@ class OrphanedProcesses < ChapterBase
   def initialize
     @title = 'Chapter 12: Orphaned Processes'
   end
+
+  def print_content
+    puts 'When a parent process dies, the child does not exit necessarily.'
+    puts 'Therefore, it is easy to create the so called "orphan" process'
+    puts 'For further knowledge, read the whole chapter of the book, as no orphan process will be created here.'
+  end
 end
