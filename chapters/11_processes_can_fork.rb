c9584b705f42b9e5dd5e66e0234ecebca234a7e9
pettil <lucas.petti@devolute.org>
2020-06-24 19:01:54 +0200

    Include chapter 11

--- chapters/11_processes_can_fork.rb
+++ chapters/11_processes_can_fork.rb
@@ -2,4 +2,30 @@ class ProcessForks < ChapterBase
   def initialize
     @title = 'Chapter 11: Processes Can Fork'
   end
+
+  def print_content
+    puts 'The fork system call allows a running process to create new processes.'
+    puts 'This new process is an exact copy of the original process.'
+    puts 'When forking, the process that initiates the fork is called the "parent" and the newly created is called the "child"'
+    puts ''
+    puts 'The child process inherits a copy of all of the memory in use by the parent, as well as any open file descriptors.'
+    puts 'The child process gets its own unique pid'
+    separator
+    puts 'Creating a child process...'
+    puts ''
+    create_fork(example_fork)
+    sleep 2
+    puts ''
+    puts 'Created child process with a block'
+  end
+
+  def create_fork(block)
+    fork { block }
+  end
+
+  def example_fork
+    separator
+    puts "-- Hello world from child process with pid #{Process.pid} --"
+    separator
+  end
 end
