7a837bb7fef568bff6cd51da732d2722fff9c7b4
pettil <lucas.petti@devolute.org>
2020-06-25 21:03:53 +0200

    Include chapter 19

--- chapters/19_spawning_terminal_processes.rb
+++ chapters/19_spawning_terminal_processes.rb
@@ -2,4 +2,30 @@ class TerminalProcesses < ChapterBase
   def initialize
     @title = 'Chapter 19: Spawning Terminal Processes'
   end
+
+  def print_content
+    puts 'Spawning a process with IO.popen. It will run the ls command:'
+    separator
+    puts IO.popen('ls').read
+    separator
+
+    puts 'Spawning with system (blocking way)'
+    system 'sleep 5'
+
+    puts 'Spawning with Process.spawn'
+    Process.spawn 'sleep 5'
+  end
 end
+
+# Examples with open3
+
+# require 'open3'
+
+# Open3.popen3('grep', 'data') { |stdin, stdout, stderr|
+#   stdin.puts "some\ndata"
+#   stdin.close
+#   puts stdout.read
+# }
+
+# Open3 will use Process.spawn when available. Options can be passed to Process.spawn like so:
+# Open3.popen3('ls', '-uhh', :err => :out) { |stdin, stdout, stderr| puts stdout.read }
