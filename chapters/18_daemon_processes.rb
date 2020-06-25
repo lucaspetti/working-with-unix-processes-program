25dda35a2c068402ab00fa59a1f109f100ff2e2b
pettil <lucas.petti@devolute.org>
2020-06-25 20:27:51 +0200

    Include chapter 18

--- chapters/18_daemon_processes.rb
+++ chapters/18_daemon_processes.rb
@@ -2,4 +2,22 @@ class DaemonProcesses < ChapterBase
   def initialize
     @title = 'Chapter 18: Daemon Processes'
   end
+
+  def print_content
+    puts 'Creating Daemon process...'
+    pid = create_daemon
+    puts "Created daemon process #{pid}"
+  end
+
+  def create_daemon
+    # What happens behind the scenes with the method Process.daemon:
+    # exit if fork
+    # Process.setsid
+    # exit if fork
+    # Dir.chdir '/'
+    # STDIN.reopen '/dev/null'
+    # STDOUT.reopen '/dev/null', 'a'
+    # STDERR.reopen '/dev/null', 'a'
+    fork { Process.daemon }
+  end
 end
