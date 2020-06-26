ea80b285f573d4f1fa2d4aab03761316a24582b8
lucaspetti <luc_petti@hotmail.com>
2020-06-26 15:02:08 +0200

    Rewrite simple server, accept port flag

--- code/simple_server.rb
+++ code/simple_server.rb
@@ -2,34 +2,42 @@ require 'socket'
 
 class SimpleServer
   def initialize
-    @server = TCPServer.new 5678
+    @server = TCPServer.new port
   end
 
   def run
-    puts "[#{Process.pid}] is listening on port 5678"
-    return http_server if http?
+    puts "[#{Process.pid}] is listening on port #{@server.addr[1]}"
+    start_server
+  end
 
+  private
+
+  def start_server
     while session = @server.accept
-      session.puts "Current time is #{Time.now}"
+      http? ? http_protocol(session) : print_time(session)
       session.close
     end
   end
 
-  def http_server
-    while session = @server.accept
-      request = session.gets
-      puts request
+  def http_protocol(session)
+    request = session.gets
+    puts request
 
-      session.print "HTTP/1.1 200\r\n" # 1
-      session.print "Content-Type: text/html\r\n" # 2
-      session.print "\r\n" # 3
-      session.print "Current time is #{Time.now}" # 4
+    session.print "HTTP/1.1 200\r\n" # 1
+    session.print "Content-Type: text/html\r\n" # 2
+    session.print "\r\n" # 3
+    print_time(session) # 4
+  end
 
-      session.close
-    end
+  def print_time(session)
+    session.puts "Current time is #{Time.now}"
   end
 
   def http?
     ARGV.include?('--http')
+  end
+
+  def port
+    ARGV.include?('--port') ? ARGV[ARGV.index('--port') + 1] : 5678
   end
 end
